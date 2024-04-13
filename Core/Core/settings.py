5ef9c47452b815964179bfd0176a5ae3d92e1a50
Pappu-kr-Mahto <pappukumar2032001@gmail.com>
2024-04-14 04:24:54 +0530

    CHORE: Changing the default db.sqllite database to Postgres database (  DB instance from render )

--- Core/Core/settings.py
+++ Core/Core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,13 +80,20 @@ WSGI_APPLICATION = 'Core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+# connection settings of postgres database
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgres://pappu:27C1ID9sEeiThQb0UaSeCU1irxYijPNY@dpg-codgco0l5elc73fno2jg-a.oregon-postgres.render.com/textsearch_db',
+        conn_max_age=600
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
