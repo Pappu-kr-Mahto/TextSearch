c290a42c12f1a463a890690d867c01cf578b279b
Pappu-kr-Mahto <pappukumar2032001@gmail.com>
2024-04-15 06:02:19 +0530

    REFACTOR:  Intall gunicorn and whitenoise to host the app in render
    Changes in requirements.txt and settings

--- Core/Core/settings.py
+++ Core/Core/settings.py
@@ -48,6 +48,7 @@ AUTH_USER_MODEL = "App.CustomUserModel"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -80,20 +81,20 @@ WSGI_APPLICATION = 'Core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # connection settings of postgres database
-DATABASES = {
-    'default': dj_database_url.config(
-        default='postgres://pappu:27C1ID9sEeiThQb0UaSeCU1irxYijPNY@dpg-codgco0l5elc73fno2jg-a.oregon-postgres.render.com/textsearch_db',
-        conn_max_age=600
-    )
-}
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='postgres://pappu:27C1ID9sEeiThQb0UaSeCU1irxYijPNY@dpg-codgco0l5elc73fno2jg-a.oregon-postgres.render.com/textsearch_db',
+#         conn_max_age=600
+#     )
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
